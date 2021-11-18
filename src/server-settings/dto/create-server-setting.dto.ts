56ac0c78251568211b3ab63c744ad5fb8ba3f274
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-11-18 12:31:27 +0100

    Create create-server-setting.dto.ts

---
+++ src/server-settings/dto/create-server-setting.dto.ts
@@ -0,0 +1,19 @@
+export class CreateServerSettingDto {
+  embedColor?: string;
+
+  prefix?: string;
+
+  supportCategoryId?: string;
+
+  warningRoleId?: string;
+
+  adminRoleId?: string;
+
+  guestRoleId?: string;
+
+  everyoneRoleId?: string;
+
+  suggestionChannelId?: string;
+
+  welcomeChannelId?: string;
+}
