e58c4a5e0f252d8567444d11dfc4456afec3f9da
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-11-18 12:31:59 +0100

    Create constants.ts

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+export const SERVER_PORT = 'SERVER_PORT';
+
+export const TYPEORM_CONFIG = 'database.config';
+export const DATABASE_PORT = 'DATABASE_PORT';
+export const DATABASE_HOST = 'DATABASE_HOST';
+export const DATABASE_NAME = 'DATABASE_NAME';
+export const DATABASE_USERNAME = 'DATABASE_USERNAME';
+export const DATABASE_PASSWORD = 'DATABASE_PASSWORD';
